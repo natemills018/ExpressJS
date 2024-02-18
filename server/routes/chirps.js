71f49e641b1f4bb381fcc5d7ca33518847bd2653
Nate Mills <natemills018@gmail.com>
2024-02-18 15:25:40 -0500

    Added Post and Delete methods

--- server/routes/chirps.js
+++ server/routes/chirps.js
@@ -1,20 +1,34 @@
 const express = require('express');
 const chirpsStore = require('../../chirpstore');
+
 let router = express.Router();
 
 router.get('/:id?', (req, res) => {
     let id = req.params.id
-    if(id) {
+    if (id) {
         res.json(chirpsStore.GetChirp(id))
     } else {
         res.send(chirpsStore.GetChirps());
     }
-    
+
 })
 
 router.post('/', (req, res) => {
     chirpsStore.CreateChirp(req.body);
     res.sendStatus(200)
-;})
+        ;
+})
+
+router.put('/:id', (req, res) => {
+    let id = req.params.id
+    chirpsStore.UpdateChirp(id, req.body)
+    res.json({ message: "Updated Successfully"})
+})
+
+router.delete('/:id', (req,res) => {
+    let id = req.params.id
+    chirpsStore.DeleteChirp(id)
+    res.json({ message: "Id Deleted Successfully"})
+})
 
 module.exports = router
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,11 +1,11 @@
 const express = require('express');
-const chirpsRouter = require('./chirp');
+const chirpsRouter = require('./chirps');
 const usersRouter = require('./users');
 
 let router = express.Router();
 
-router.use('/api/chirps', chirpsRouter);
-router.use('/api/users', usersRouter);
+router.use('/chirps', chirpsRouter);
+router.use('/users', usersRouter);
 
 
 module.exports = router;
